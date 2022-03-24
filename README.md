d792c74bba7c733e7757475b66f8105bf9e50c97
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-03-25 02:05:42 +0600

    updated readme file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Getting Started with Create React App
+# How react work(updated)
+## I will read [reactjs.org completely](https://reactjs.org/docs/getting-started.html)
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadComments></LoadComments>
+     <District name='Noahakali' special='bivag'></District>
+     <District name='Brammonbriya' special='lati sota'></District>
+     <District name='Comilla' special='Moyna and moti'></District>
     </div>
   );
 }
+function LoadComments(){
+  const [comments,setComments] = useState([])
 
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => setComments(data))
+  },[])
+  return (
+    <div>
+      <h1>Comments: {comments.length}</h1>
+      {
+        comments.map(comment => console.log(comment))
+      }
+      {
+        comments.map(comment => <Comment name={comment.name} body={comment.body}></Comment>)
+      }
+    </div>
+  )
+};
+function Comment(props){
+  return (
+    <div style={{backgroundColor: 'lightblue',margin:'10px',padding:'15px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Body: {props.body}</p>
+    </div>
+  )
+}
+const districtStyle = {
+  backgroundColor: 'lightpink',
+  margin: '15px',
+  padding: '15px',
+  border: '1px solid gray'
+}
+function District(props){
+  const [power,setPower] = useState(1)
+
+  const boostPower = () => setPower(power * 2);
+  return (
+    <div style={districtStyle}>
+      <h1>Name: {props.name}</h1>
+      <p>Speciality: {props.special}</p>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower}>Boost the power</button>
+    </div>
+  )
+}
 export default App;
